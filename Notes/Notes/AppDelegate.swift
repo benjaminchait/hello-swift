24cf5cbf6a0bd47d1854c79bbedbc533cb153ea5
Benjamin Chait <benjamin@simple.com>
2016-06-21 21:25:43 -0700

    create Notes

---
+++ Notes/Notes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by Benjamin Chait on 6/21/16.
+//  Copyright © 2016 Benjamin Chait. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
